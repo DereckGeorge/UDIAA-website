a8a3f9c3cbec5a26d17ef5becfbc22df79a0b278
KelvinHussle <kelvinkalinga99@gmail.com>
2023-10-17 21:34:32 +0400

    chore: event images

--- udiaa-website/src/components/AllGalleryTabs/EventGallery.js
+++ udiaa-website/src/components/AllGalleryTabs/EventGallery.js
@@ -3,38 +3,9 @@ import clock from '../images/clock.png';
 import location from '../images/location.png';
 import './Eventgallery.css'
 import { Container } from 'react-bootstrap';
-import { Gallery } from "react-grid-gallery";
+import EventImages from '../EventImages/EventImages';
 
 
-const images = [
-    {
-       src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-       width: 320,
-       height: 174,
-       caption: "After Rain (Jeshu John - designerspics.com)",
-    },
-    {
-       src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-       width: 320,
-       height: 212,
-       tags: [
-          { value: "Ocean", title: "Ocean" },
-          { value: "People", title: "People" },
-       ],
-       alt: "Boats (Jeshu John - designerspics.com)",
-    },
-    {
-       src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-       width: 320,
-       height: 212,
-    },
-    {
-        src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-        width: 320,
-        height: 212,
-     },
- ];
- 
 
 const EventsInfo = [
     {
@@ -164,10 +135,7 @@ const Events = () => {
                     </div>
                 </div>
                 </div>
-                <Container>
-                <Gallery images={images} 
-               />
-                </Container>
+                <EventImages/>
             </Container>
         ));
     };
