7eecfb931c6b900996c436a534b4544f92905f7c
Kelvinhussle <kelvinkalinga99@gmail.com>
2024-05-01 16:10:27 +0300

    finalizing the UI

--- udiaa-website/src/components/header/header.js
+++ udiaa-website/src/components/header/header.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './header.css';
 import { useNavigate } from "react-router-dom";
-import HeadImage from '../images/image1.webp'
-import { Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight} from '@fortawesome/free-solid-svg-icons'
 
@@ -13,15 +11,15 @@ const Header = () => {
     navigate("/about");
   };
   return (
-    <Container fluid>
-    <div class=" fluid-container">
-    <img class="image" src={HeadImage} alt="Blurred Image"/>
-    <div class="overlay">
-      <h1 className='hh'>Welcome to UDIAA</h1>
+    <>
+<section class="bg-image d-flex">
+<div class="text-overlay">
+      <div><h1>Welcome to UDIAA</h1></div>
+      <div><p >WE SHAPE YOUR FUTURE TODAY</p></div>
       <div className="learnmorebutton" onClick={handleClick}><div>Learn More</div><div><FontAwesomeIcon icon={faArrowRight} /></div></div>
     </div>
-</div >
-  </Container>
+</section >
+  </>
   );
 };
 
